b853a03892be278dca336aaf63549c3d48b78a4f
guojufeng <git.jd.com>
2019-11-30 16:39:07 +0800

    [+]bs movie
    爬虫 - 爬取电影信息和下载地址

--- Python/Robots/3-movie.py
+++ Python/Robots/3-movie.py
@@ -11,22 +11,39 @@
 
 # 思路一：先爬取最小共同父级标签 <li>，然后针对每一个父级标签，提取里面的序号/电影名/评分/推荐语/链接。
 
-import requests, random, bs4
-
-for x in range(10):
-    url = 'https://movie.douban.com/top250?start=' + str(x*25) + '&filter='
-    res = requests.get(url)
-    bs = bs4.BeautifulSoup(res.text, 'html.parser')
-    bs = bs.find('ol', class_="grid_view")
-    for titles in bs.find_all('li'):
-        num = titles.find('em',class_="").text
-        #查找序号
-        title = titles.find('span', class_="title").text
-        #查找电影名
-        tes = titles.find('span',class_="inq").text
-        #查找推荐语
-        comment = titles.find('span',class_="rating_num").text
-        #查找评分
-        url_movie = titles.find('a')['href']
-
-        print(num + '.' + title + '——' + comment + '\n' + '推荐语：' + tes +'\n' + url_movie)
+import requests
+import random
+import bs4
+movieDict = {}
+try:
+    for x in range(10):
+      url = 'https://movie.douban.com/top250?start=' + str(x*25) + '&filter='
+      headers = {
+          "Host": "movie.douban.com",
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+      }
+      res = requests.get(url, headers=headers)
+      bs = bs4.BeautifulSoup(res.text, 'html.parser')
+      movieList = bs.find('ol', class_="grid_view").find_all('li')
+      for movie in movieList:
+        num = movie.find('em', class_="").text  # 查找序号
+        title = movie.find('span', class_="title").text  # 查找电影名
+        tes = movie.find('span', class_="inq")
+        if tes is None:
+          tes = '无'
+        else:
+          tes = tes.text
+        score = movie.find('span', class_="rating_num").text  # 查找评分
+        url_movie = movie.find('a')['href']  # 查找地址
+        movieDict[title] = {
+            "id": num,
+            "评分": score,
+            "推荐语": tes,
+            "地址": url_movie
+        }
+        print(num + '.' + title + '——' + score +
+              '\n' + '推荐语：' + tes + '\n' + url_movie)
+        print('====')
+except:
+    print('代码错误，操作失败')
+print(movieDict)
--- Python/Robots/3-movie2.py
+++ Python/Robots/3-movie2.py
@@ -1,1 +1,41 @@
 # 思路二：分别提取所有的序号/所有的电影名/所有的评分/所有的推荐语/所有的链接，然后再按顺序一一对应起来。
+
+import requests
+import random
+import bs4
+movieDict = {}
+try:
+  for x in range(10):
+    url = 'https://movie.douban.com/top250?start=' + str(x*25) + '&filter='
+    headers = {
+        "Host": "movie.douban.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+    }
+    res = requests.get(url, headers=headers)
+    bs = bs4.BeautifulSoup(res.text, 'html.parser')
+    movieList = bs.find('ol', class_="grid_view")
+    num = movieList.find_all('em', class_="")  # 查找所有序号
+    score = movieList.find_all('span', class_="rating_num")  # 查找所有评分
+    title_box = movieList.find_all('div',class_="hd")  # 查找地址
+    # 描述语比较特殊，有的没有这个标签，不好对应，所以找到一定有的父元素即可
+    infoBox = movieList.find_all('div', class_="bd")
+    for movieNum in range(len(title_box)):
+      title = title_box[movieNum].find('span', class_="title").text  # 查找所有电影名
+      url_movie = title_box[movieNum].find('a')['href']
+      tes = infoBox[movieNum].find('span', class_="inq")
+      if tes is None:
+        tes = '无'
+      else:
+        tes = tes.text
+      movieDict[title] = {
+          "id": num[movieNum].text,
+          "评分": score[movieNum].text,
+          "推荐语": tes,
+          "地址": url_movie
+      }
+      print(num[movieNum].text + '.' + title + '——' + score[movieNum].text +
+            '\n' + '推荐语：' + tes + '\n' + url_movie)
+      print('====')
+except:
+    print('代码错误，操作失败')
+print(movieDict)
