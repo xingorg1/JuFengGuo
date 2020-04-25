6ad38315a0d32d37c98c76080592ffc8942d4201
guojufeng <guojufeng@jd.com>
2020-04-25 11:13:51 +0800

    [+]豆瓣电影爬虫库

---
+++ EmploymentClass/10pacakge/npm/spiderMan/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: @Guojufeng 
+ * @Date: 2020-04-25 11:12:35 
+ * @Last Modified by: @Guojufeng
+ * @Last Modified time: 2020-04-25 11:13:14
+ */
+const dealMovieInfo = require('./spider')
+module.exports = dealMovieInfo
