419de73d1ae0f7dc86c10c9e664c8588785e3b1b
guojufeng <guojufeng@jd.com>
2020-05-21 14:52:37 +0800

    【html】本地起服务

--- EmploymentClass/5HTML5/myServer/server.js
+++ EmploymentClass/5HTML5/myServer/server.js
@@ -1,8 +1,8 @@
 /*
  * @Author: @Guojufeng 
  * @Date: 2019-05-28 19:14:32 
- * @Last Modified by: @Guojufeng
- * @Last Modified time: 2019-05-28 19:46:37
+ * @Last Modified by: guojufeng
+ * @Last Modified time: 2020-05-21 14:45:19
  * 服务器文件
  */
 var express = require('express');// 引入node的express
@@ -11,4 +11,6 @@ var app = new express();// 注册express，构建app对象。express默认访问
 
 app.use(express.static("./page"));// 设置一个静态文件的路径（当前文件夹下的page路径）
 
-app.listen(9310);//9310为端口号。注意端口号尽量大于8000或者等于80，不写就是默认的80。 80是所有页面的默认端口
+app.listen(9310, () => {
+  console.log('服务启动成功')
+});//9310为端口号。注意端口号尽量大于8000或者等于80，不写就是默认的80。 80是所有页面的默认端口
