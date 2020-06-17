b81fbefd743786a65624f0815fcdb91b774fd060
guojufeng <guojufeng@jd.com>
2020-06-17 22:20:23 +0800

    [+]【typescript】模块化

--- advanceCourse/typescript/1-basics/src/1-1-index.ts
+++ advanceCourse/typescript/1-basics/src/1-1-index.ts
@@ -1,3 +1,8 @@
-let myname = '12';
-myname = `hahah${myname}`;
-console.log(myname,'gjf3');
+// 导入练习
+import module02, {moduleName, funcName} from './2-module'
+console.log(module02, moduleName)
+console.log(module02.name, funcName)
+// 其他练习
+// let myname = '12';
+// myname = `hahah${myname}`;
+// console.log(myname,'gjf3');
---
+++ advanceCourse/typescript/1-basics/src/2-module.ts
@@ -0,0 +1,9 @@
+// 导出练习 - 导出固定模块
+export const moduleName = '模块导出练习'
+export function funcName(a: number) {
+  return a+1
+}
+// 导出练习 - 导出默认模块
+export default {
+  name: '默认的导出'
+}
