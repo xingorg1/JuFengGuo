36494b7dd8b12acf5b7a22e67311ee17fad661ed
guojufeng <guojufeng@jd.com>
2020-06-19 00:08:52 +0800

    [+]【typescript+react】函数组件的严格模式

--- advanceCourse/typescript/japplet/src/App.tsx
+++ advanceCourse/typescript/japplet/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import { CountComp } from './components/CountComp';
+const {log} = console
+let num = 1
 function App() {
+  
   return (
     <div className="App">
       <h1>三子棋</h1>
+      <CountComp num={num} countHandle={(num1) => {
+        num = num1
+        log(num1, num)
+      }}>
+        <h1>我是CountComp的内容部分，会被props的children属性接收。</h1>
+        {/* children里传递纯文本和jsx还不一样呢 */}
+      </CountComp>
     </div>
   );
 }
