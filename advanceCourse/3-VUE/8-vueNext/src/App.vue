52a0a0c9e3812981c7a560cb69aa199429109492
guojufeng <guojufeng@jd.com>
2020-10-31 21:37:31 +0800

    vue：vue3引入element2-失败

--- advanceCourse/3-VUE/8-vueNext/src/App.vue
+++ advanceCourse/3-VUE/8-vueNext/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>哈哈哈</h1>
+  <!-- <el-button>vue3引入Element2</el-button> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 console.log(this)
 
 export default {
--- advanceCourse/3-VUE/8-vueNext/src/main.js
+++ advanceCourse/3-VUE/8-vueNext/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
 import './index.css'
 console.log(this)
+console.log(ElementUI)
 createApp(App).mount('#app')
