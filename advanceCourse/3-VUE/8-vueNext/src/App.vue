ce6563a3c4a85c4a7be9b132c38c0d10051348f1
guojufeng <guojufeng@jd.com>
2020-12-01 22:42:36 +0800

    vue: Axe-ui使用demo

--- advanceCourse/3-VUE/8-vueNext/src/App.vue
+++ advanceCourse/3-VUE/8-vueNext/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <h1>哈哈哈</h1>
-  <!-- <el-button>vue3引入Element2</el-button> -->
+  <axe-button icon="axe-icon-love" type="danger">Axe使用成功啦！</axe-button>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 console.log(this)
 
 export default {
--- advanceCourse/3-VUE/8-vueNext/src/main.js
+++ advanceCourse/3-VUE/8-vueNext/src/main.js
@@ -1,7 +1,24 @@
+import './index.css'
+// 全局引入axe-ui
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import './index.css'
-console.log(this)
-console.log(ElementUI)
-createApp(App).mount('#app')
+import Axe from 'axe-ui'
+import 'axe-ui/dist/axe.css'
+createApp(App).use(Axe).mount('#app')
+
+
+// 局部引入axe-ui
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import {
+//   Input,
+//   Button
+//   // ...其他组件
+// } from 'axe-ui'
+// console.log(Button)
+// // import 'axe-ui/dist/css/input/style.css'
+// // import 'axe-ui/dist/css/button/style.css'
+// let app = createApp(App)
+// app.component(Input.name, Input)
+// app.component(Button.name, Button)
+// app.mount('#app')
