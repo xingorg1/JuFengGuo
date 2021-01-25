9e20b6bc1773a71e879db8ef501c160f80c16c72
guojufeng <guojufeng@jd.com>
2021-01-26 07:37:52 +0800

    node: fs模块

--- advanceCourse/5-node/API-fs/writeFile.js
+++ advanceCourse/5-node/API-fs/writeFile.js
@@ -27,7 +27,7 @@ fs.writeFile(pathUrl, content, { // 兼容性写法
 // 一个不存在的路径，stat会报错。要用try、catch捕获。
 async function testStat() {
   try {
-    const result = await fs.promises.stat(pathUrl1) 
+    const result = await fs.promises.stat(pathUrl1)
     console.log(10, result)
   } catch (error) {
     console.log(12, error)
@@ -57,14 +57,14 @@ fs.writeFile(pathDirectory, '不存在的目录会导致错误', 'utf-8', (err) 
 })
 
 // 写入时的容错处理
-try{
+try {
   const isFile = fs.statSync(pathDirectory);
   isFile && fs.writeFile(pathDirectory, '不存在的目录会导致错误', 'utf-8', (err) => {
     console.log('目录不存在，出错啦！', err) //这样就不会走到这一行了
   })
   console.log('是否是一个文件夹', isFile.isDirectory())
 }
-catch(error) {
-  console.log('抛出错误', error)
+catch (error) {
+  console.log('抛出错误~', error) // 抛出错误~ ReferenceError: pathDirectory is not defined
 }
 
