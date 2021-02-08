6dddd4bbdce10d4a32a4d19686b34023cfda3f15
xingorg1 <xing.org1@outlook.com>
2021-02-08 17:06:51 +0800

    feat: body-parser 解析post请求消息体

--- advanceCourse/express/netApi/index.js
+++ advanceCourse/express/netApi/index.js
@@ -4,6 +4,9 @@ const app = express()
 const port = 3000
 const fs = require('fs')
 const path = require('path')
+const bodyParser = require('body-parser') // express接受post参数需要引入一个核心模块 body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // 激活了"trust proxy" 设置,Express 就会知道它在一个代理的后面.比如Nginx反向代理之后的express服务
 app.enable('trust proxy') // 激活反向代理 将设置项 括号中参数 的值设为 true.
@@ -49,6 +52,15 @@ app.get('/', (req, res) => {
   res.send(indexHtml)
 })
 app.get('/getData', (req, res) => {
+  // log('请求', req)
+  res.send({
+    code: 200,
+    flag: true,
+    info: '这是成功的文字',
+    msg: 'success'
+  })
+})
+app.post('/getData', (req, res) => {
   // log('请求', req)
   res.send({
     code: 200,
