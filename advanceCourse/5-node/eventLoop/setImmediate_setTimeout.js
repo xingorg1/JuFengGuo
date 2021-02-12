9cab9f0f77a55ccd96a574e141ff89ceb6b043e5
xingorg1 <xing.org1@outlook.com>
2021-02-12 11:01:10 +0800

    node: setImmediate and setTimeout

--- advanceCourse/5-node/eventLoop/setImmediate_setTimeout.js
+++ advanceCourse/5-node/eventLoop/setImmediate_setTimeout.js
@@ -4,8 +4,8 @@ console.time('setImmediate');
 
 function test() {
   if (i < 1000) {
-    // setTimeout(test, 0)
-    setImmediate(test)
+    // setTimeout(test, 0) // 约1.334s
+    setImmediate(test) // 约22.122ms
     i++
   } else {
     console.timeEnd('setImmediate');
