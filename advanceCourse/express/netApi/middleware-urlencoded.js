2ea90266dfe6f0c85830391c9345f2ee96302fe7
xingorg1 <xing.org1@outlook.com>
2021-02-20 02:24:33 +0800

    fix: urlencoded middleware

--- advanceCourse/express/netApi/middleware-urlencoded.js
+++ advanceCourse/express/netApi/middleware-urlencoded.js
@@ -6,7 +6,9 @@ app.use(express.static(path.resolve(__dirname, 'public')))
 
 // 使用自己仿写的
 const myUrlencoded = require('./rewriteMiddleware/urlencodedMiddleware')
-app.use(myUrlencoded)
+app.use(myUrlencoded({
+  a: 123
+}))
 
 // 使用原生提供的
 // app.use(express.urlencoded({
@@ -15,7 +17,6 @@ app.use(myUrlencoded)
 // }))
 // 当请求的Content-type与type类型（"application/x-www-form-urlencoded"）匹配，中间件就会进行处理，将请求的数据通过流的方式进行整合，最后将数据存储到req.body中
 app.post('/api/person', (req) => {
-  console.log('body'); // 得到：{ name: 'xing.org1^', age: '18' }
   console.log(req.body); // 得到：{ name: 'xing.org1^', age: '18' }
 })
 
