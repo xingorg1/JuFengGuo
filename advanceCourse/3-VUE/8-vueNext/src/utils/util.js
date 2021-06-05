834e2a36d86dc7c3e801ee93476275101963059c
xingorg1 <xing.org1@outlook.com>
2021-06-05 23:11:29 +0800

    styles: vue3-todolist 格式化

--- advanceCourse/3-VUE/8-vueNext/src/utils/util.js
+++ advanceCourse/3-VUE/8-vueNext/src/utils/util.js
@@ -1,35 +1,35 @@
 export const wls = {
-    /* localStorage 封装 */
-    ls: window.localStorage,
-    getItem(key) {
-        try {
-            let key1 = key && this.ls.getItem(key);
-            if (key1) {
-                return JSON.parse(key1);
-            } else {
-                return null;
-            }
-        } catch (err) {
-            return null;
-        }
-    },
-    setItem(key, val) {
-        this.ls.setItem(key, JSON.stringify(val));
-    },
-    removeItem(key) {
-        this.ls.removeItem(key);
-    },
-    keys() {
-        return this.ls.keys();
-    },
-    clear() {
-        this.ls.clear();
+  /* localStorage 封装 */
+  ls: window.localStorage,
+  getItem(key) {
+    try {
+      let key1 = key && this.ls.getItem(key);
+      if (key1) {
+        return JSON.parse(key1);
+      } else {
+        return null;
+      }
+    } catch (err) {
+      return null;
     }
+  },
+  setItem(key, val) {
+    this.ls.setItem(key, JSON.stringify(val));
+  },
+  removeItem(key) {
+    this.ls.removeItem(key);
+  },
+  keys() {
+    return this.ls.keys();
+  },
+  clear() {
+    this.ls.clear();
+  }
 }
 
 export const md5ID = () => {
-    let ramdomStr = Math.random().toString(16)
-    return new Date().getTime() + ramdomStr.substr(ramdomStr.length - 6)
+  let ramdomStr = Math.random().toString(16)
+  return new Date().getTime() + ramdomStr.substr(ramdomStr.length - 6)
 }
 
 export const zeroFill = (str = '') => {
