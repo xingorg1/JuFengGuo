a86c33e58d81a9f096a73b748d4e7801d432bf5d
xingorg1 <xing.org1@outlook.com>
2021-06-05 19:21:04 +0800

    vue3: Initialize the TodoList

--- advanceCourse/3-VUE/8-vueNext/src/App.vue
+++ advanceCourse/3-VUE/8-vueNext/src/App.vue
@@ -1,14 +1,55 @@
 <template>
-  <h1>哈哈哈</h1>
-  <axe-button icon="axe-icon-love" type="danger">Axe使用成功啦！</axe-button>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        class="new-todo"
+        autofocus=""
+        autocomplete="off"
+        placeholder="What needs to be done?"
+        v-model="newTodoRef"
+        @keyup.enter="addTodo"
+      />
+    </header>
+    <section class="main">
+      <input id="toggle-all" class="toggle-all" type="checkbox" />
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li class="todo">
+          <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label>学习composition api</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" type="text" />
+        </li>
+      </ul>
+    </section>
+    <footer class="footer">
+      <span class="todo-count">
+        <strong>1</strong>
+        <span>items left</span>
+      </span>
+      <ul class="filters">
+        <li><a href="#/all" class="selected">All</a></li>
+        <li><a href="#/active" class="">Active</a></li>
+        <li><a href="#/completed" class="">Completed</a></li>
+      </ul>
+      <button class="clear-completed" style="display: none">
+        Clear completed
+      </button>
+    </footer>
+  </section>
+  <hr />
+  <axe-button icon="axe-icon-love" type="danger" style="float: right"
+    >Axe使用成功啦！喜大普奔～</axe-button
+  >
 </template>
 
 <script>
-console.log(this)
-
 export default {
-  setup(){
-    console.log(this)
-  }
+  setup() {
+    // console.log(this)
+  },
 };
 </script>
