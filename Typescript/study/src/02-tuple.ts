25f2e8d87e8cd6c81d59f1d671680648e19d9167
xingorg1 <xing.org1@outlook.com>
2022-06-01 18:52:34 +0800

    feat: ts study

---
+++ Typescript/study/src/02-tuple.ts
@@ -0,0 +1,7 @@
+// 元祖类型
+let tuple: [string, number, boolean] = ['gjf', 10, true]
+// 像元组中增加数据，只能增加元组中存放的类型
+tuple.push('小石头')
+console.log(tuple)
+console.log(tuple[0].length)
+console.log(tuple[1].toFixed(2))
